ee1fa180c9a761ee862c86f9a3d667d5b5d7b490
Atul Singh <asatulsingh20@gmail.com>
2025-05-27 01:29:23 +0530

    add icon component

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,5 @@
 export { Button } from "./components/button";
 export { Loading } from "./components/loading";
 export { Link } from "./components/link";
+export { Tab } from "./components/tab";
+export { Icon } from "./components/icon";
